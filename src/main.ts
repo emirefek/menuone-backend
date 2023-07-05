d466e38c69d099c6147db9fb9f3745a6628e9f33
emirefek <emirefek@gmail.com>
2023-07-05 22:44:48 +0300

    port env variable

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const mainBackend = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
-  await mainBackend.listen(3000);
+  const configService = mainBackend.get(ConfigService);
+  const port = configService.get("PORT");
+  await mainBackend.listen(port || 3000);
 }
 bootstrap();
