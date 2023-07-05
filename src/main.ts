32ef85b13cdcb8ac3c153eb2ce3ec8708b43b9fd
emirefek <emirefek@gmail.com>
2023-07-06 01:00:22 +0300

    nestjs prod

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { ConfigService } from "@nestjs/config";
 
+process.title = "menudrop-backend";
+
 async function bootstrap() {
   const mainBackend = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
   const configService = mainBackend.get(ConfigService);
-  const port = configService.get("PORT");
-  await mainBackend.listen(port || 3000);
+  const port = configService.get("PORT") || 3000;
+  console.log(`Listening on port ${port}`);
+  await mainBackend.listen(port);
 }
 bootstrap();
